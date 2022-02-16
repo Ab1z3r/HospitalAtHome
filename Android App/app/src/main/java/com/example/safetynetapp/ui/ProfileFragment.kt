decace801c394c88d879d298db4e54fe9387e97d
rhit-kuznicmd <kuznicmd@rose-hulman.edu>
2022-02-16 15:09:08 -0500

    Added Profile and Settings Page UI

--- Android App/app/src/main/java/com/example/safetynetapp/ui/ProfileFragment.kt
+++ Android App/app/src/main/java/com/example/safetynetapp/ui/ProfileFragment.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.safetynetapp.databinding.FragmentProfileBinding
-import com.example.safetynetapp.ui.gallery.GalleryViewModel
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
+//    private lateinit var model: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +19,12 @@ class ProfileFragment : Fragment() {
     ): View {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
 
+        initializeTextFields()
+
         return binding.root
+    }
+
+    fun initializeTextFields() {
+
     }
 }
