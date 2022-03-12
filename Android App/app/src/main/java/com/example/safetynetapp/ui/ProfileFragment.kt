faba782eaed4d4fc9ce532e7328f6d448cfa8777
rhit-kuznicmd <kuznicmd@rose-hulman.edu>
2022-03-11 20:01:56 -0500

    Started working on Profile Page Functionality

--- Android App/app/src/main/java/com/example/safetynetapp/ui/ProfileFragment.kt
+++ Android App/app/src/main/java/com/example/safetynetapp/ui/ProfileFragment.kt
@@ -1,16 +1,19 @@
 package com.example.safetynetapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.safetynetapp.databinding.FragmentProfileBinding
+import com.example.safetynetapp.models.UserViewModel
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
-//    private lateinit var model: UserViewModel
+    private lateinit var model: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,13 +21,16 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
+        model = ViewModelProvider(requireActivity()).get(UserViewModel::class.java)
 
-        initializeTextFields()
+        updateView()
 
         return binding.root
     }
 
-    fun initializeTextFields() {
-
+    fun updateView() {
+        with (model.user!!) {
+            Log.d("TAG", "in profile update view")
+        }
     }
 }
