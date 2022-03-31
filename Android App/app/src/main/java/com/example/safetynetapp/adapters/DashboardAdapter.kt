fa3c094416ee333a81910ad3f8138a20d14d8564
haoy1 <haoy1@rose-hulman.edu>
2022-03-31 12:50:08 -0400

    TODO

--- Android App/app/src/main/java/com/example/safetynetapp/adapters/DashboardAdapter.kt
+++ Android App/app/src/main/java/com/example/safetynetapp/adapters/DashboardAdapter.kt
@@ -1,46 +1,28 @@
 package com.example.safetynetapp.adapters
 
-import android.app.PendingIntent.getActivity
-import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.safetynetapp.HomeActivity
 import com.example.safetynetapp.R
 import com.example.safetynetapp.models.DashboardViewModel
 import com.example.safetynetapp.models.Vital
-import com.example.safetynetapp.ui.ChartActivity
 import com.example.safetynetapp.ui.DashboardFragment
-import com.google.firebase.Timestamp
-import com.example.safetynetapp.MainActivity
 import com.example.safetynetapp.models.UserViewModel
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.fitness.Fitness
-import com.google.android.gms.fitness.FitnessOptions
-import com.google.android.gms.fitness.data.DataPoint
-import com.google.android.gms.fitness.data.DataType
-import java.time.LocalDateTime
-import java.time.ZoneId
+
+import androidx.navigation.fragment.NavHostFragment.findNavController
 
 
 class DashboardAdapter(val fragment: DashboardFragment, val parentActivity: HomeActivity): RecyclerView.Adapter<DashboardAdapter.DashboardViewHolder>() {
     val model = ViewModelProvider(fragment.requireActivity()).get(DashboardViewModel::class.java)
     val usermodel = ViewModelProvider(fragment.requireActivity()).get(UserViewModel::class.java)
+    val dashFragment = DashboardFragment();
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): DashboardAdapter.DashboardViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DashboardAdapter.DashboardViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.dashboard_card, parent, false)
         return DashboardViewHolder(view)
     }
@@ -63,8 +45,20 @@ class DashboardAdapter(val fragment: DashboardFragment, val parentActivity: Home
                 // TODO: Create updatePos method in view model
                 //model.updatePos(adapterPosition)
                 // TODO: Navigate to proper detail page
-                val intent = Intent(itemView.getContext(), ChartActivity::class.java)
-                itemView.getContext().startActivity(intent)
+//                val intent = Intent(itemView.getContext(), ChartActivity::class.java)
+//                itemView.getContext().startActivity(intent)
+
+//                val cfragment: Fragment = ChartActivity()
+//                val dfragment: Fragment = DashboardFragment()
+//                val ft : FragmentTransaction = (fragment.requireActivity()).supportFragmentManager.beginTransaction()
+//                ft.replace(R.id.fragment_dashborad,cfragment)
+//                //ft.attach(cfragment)
+
+ //               ft.commit()
+
+            //               findNavController(fragment).navigate(R.id.line_chart)
+
+
             }
         }
 
